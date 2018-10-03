b884e25c2796a7c3e5c04c259b21c16d6b4d526c
steven243 <43686665+steven243@users.noreply.github.com>
2018-10-03 13:58:56 -0500

    Add files via upload

---
+++ predecir.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def clasificar():
+     
+    file = open("modelo_knn.mod", "rb")
+    knn = pickle.load(file)
+    file.close()
+     
+     
+    sl = 4.2
+    sw = 3.8
+    pl = 0.9
+    pw = 4.4
+     
+    datos = np.array([[sl, sw, pl, pw]])
+    predictions = knn.predict(datos)
+    
+    return predictions[0] 
+
+if __name__ == '__main__':
+   app.run()
